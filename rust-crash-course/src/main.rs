fecf2567093e447cc5fb91018901124fb0babbbb
Sharjeel (Personal) <sharjeelpersonal@Sharjeels-MacBook-Pro-2.local>
2024-09-29 17:28:05 -0700

    Learned about floating points, integers, and operators

--- rust-crash-course/src/main.rs
+++ rust-crash-course/src/main.rs
@@ -1,12 +1,46 @@
 #![deny(clippy::all)]
 
 fn main() {
-    println!("Hello, world!");
 
-    let first_name: &str = "Foo";
 
-    println!("Hello {}!", first_name);
 
-    let _testing: &str = "testing rust compiler";
-    println!(_testing)
+    // Data Types
+
+    //* Integers iX uX*/
+
+    // Signed values can be negative or positive, most use cases are i32
+    let _age: i32 = -20;
+
+    // Unsigned values cannot be negative
+    let _unsigned: u8 = 20;
+
+    let _unsigned_data_type = 20u8;
+
+
+    let _population = 62000000;
+    let _population_underscore = 62_000_000;
+
+    let _red = 0xFA;
+    let _rgb = 0xFF0000;
+
+
+    //* Floating Point Values f32 f64 */
+
+    let _floating: f32 = 5.5;
+
+    let _distance_in_km = 5.5f32;
+
+    //* Operators */
+
+    let distance1 = 5.5;
+    let distance2 = 6.5;
+    let distance3 = 10.2;
+
+    let _total_distance = distance1 + distance2 + distance3;
+
+
+
+
+
+
 }
